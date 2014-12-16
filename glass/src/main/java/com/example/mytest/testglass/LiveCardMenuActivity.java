9da57fb0f2ae2004d489d5b7d41acaa6da7a6074
MOREL Pierre-Joseph <PJMOREL@CEGID.FR>
2014-12-16 09:16:03 +0100

    Résolution Pb de connexion

--- glass/src/main/java/com/example/mytest/testglass/LiveCardMenuActivity.java
+++ glass/src/main/java/com/example/mytest/testglass/LiveCardMenuActivity.java
@@ -2,16 +2,35 @@ package com.example.mytest.testglass;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Debug;
+import android.os.Message;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.net.http.AndroidHttpClient;
+import android.widget.Toast;
 
+import org.apache.http.Header;
+import org.apache.http.HeaderIterator;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.ProtocolVersion;
+import org.apache.http.RequestLine;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpParams;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
 
 /**
- * A transparent {@link Activity} displaying a "Stop" options menu to remove the {@link LiveCard}.
+ * A transparent {@link Activity} displaying a "Stop" options menu to remove the {@link com.example.mytest.testglass.LiveCardMenuActivity}.
  */
 public class LiveCardMenuActivity extends Activity {
 
@@ -27,21 +46,11 @@ public class LiveCardMenuActivity extends Activity {
         getMenuInflater().inflate(R.menu.live_card, menu);
         return true;
     }
-
-    @Override
+  @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_httpget:
-                try
-                {
-                    android.net.http.AndroidHttpClient httpClient = android.net.http.AndroidHttpClient.newInstance("Android");
-                    HttpResponse response = httpClient.execute( new HttpGet("http://192.168.0.26/kanguru/api/store/currentproduct.json"));
-                    httpClient.close();
-                }
-                catch (Exception e)
-                {
-                    return false;
-                }
+                new HttpAsyncTask().execute("http://192.168.0.26/kanguru/api/product/current");
                 return true;
             case R.id.action_stop:
                 // Stop the service which will unpublish the live card.
@@ -50,6 +59,70 @@ public class LiveCardMenuActivity extends Activity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    public static String GET(String url){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if(inputStream != null)
+                result = convertStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        return result;
+    }
+
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return GET(urls[0]);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private static String convertStreamToString(InputStream is) {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append((line + "\n"));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
     }
 
     @Override
